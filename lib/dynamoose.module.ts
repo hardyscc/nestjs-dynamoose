28e5e15d2d16a1103efc9a580e411a01ff605f54
Hardys <hardyscc@gmail.com>
2020-03-21 22:24:45 +0800

    feat: add root and feature async support

--- lib/dynamoose.module.ts
+++ lib/dynamoose.module.ts
@@ -1,8 +1,15 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, flatten, Module } from '@nestjs/common';
 import { DynamooseCoreModule } from './dynamoose-core.module';
-import { createDynamooseProviders } from './dynamoose.providers';
+import {
+  createDynamooseAsyncProviders,
+  createDynamooseProviders,
+} from './dynamoose.providers';
 import { ModelDefinition } from './interfaces';
-import { DynamooseModuleOptions } from './interfaces/dynamoose-options.interface';
+import { AsyncModelFactory } from './interfaces/async-model-factory.interface';
+import {
+  DynamooseModuleAsyncOptions,
+  DynamooseModuleOptions,
+} from './interfaces/dynamoose-options.interface';
 
 @Module({})
 export class DynamooseModule {
@@ -13,10 +20,29 @@ export class DynamooseModule {
     };
   }
 
+  static forRootAsync(options: DynamooseModuleAsyncOptions): DynamicModule {
+    return {
+      module: DynamooseModule,
+      imports: [DynamooseCoreModule.forRootAsync(options)],
+    };
+  }
+
   static forFeature(models: ModelDefinition[] = []): DynamicModule {
     const providers = createDynamooseProviders(models);
     return {
       module: DynamooseModule,
+      providers: providers,
+      exports: providers,
+    };
+  }
+
+  static forFeatureAsync(factories: AsyncModelFactory[] = []): DynamicModule {
+    const providers = createDynamooseAsyncProviders(factories);
+    const imports = factories.map(factory => factory.imports || []);
+    const uniqImports = new Set(flatten(imports));
+    return {
+      module: DynamooseModule,
+      imports: [...uniqImports],
       providers: providers,
       exports: providers,
     };
--- lib/dynamoose.providers.ts
+++ lib/dynamoose.providers.ts
@@ -1,6 +1,8 @@
+import { flatten } from '@nestjs/common';
 import { Model } from 'dynamoose';
 import { getModelToken } from './common/dynamoose.utils';
 import { ModelDefinition } from './interfaces';
+import { AsyncModelFactory } from './interfaces/async-model-factory.interface';
 
 export function createDynamooseProviders(models: ModelDefinition[] = []) {
   const providers = (models || []).map(model => ({
@@ -8,4 +10,20 @@ export function createDynamooseProviders(models: ModelDefinition[] = []) {
     useFactory: () => new Model(model.name, model.schema, model.options),
   }));
   return providers;
+}
+
+export function createDynamooseAsyncProviders(
+  modelFactories: AsyncModelFactory[] = [],
+) {
+  const providers = (modelFactories || []).map(model => [
+    {
+      provide: getModelToken(model.name),
+      useFactory: async (...args: unknown[]) => {
+        const schema = await model.useFactory(...args);
+        return new Model(model.name, schema, model.options);
+      },
+      inject: [...(model.inject || [])],
+    },
+  ]);
+  return flatten(providers);
 }
