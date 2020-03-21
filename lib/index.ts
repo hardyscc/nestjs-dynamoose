da625411c1c480f951dfa99060db8b1e0789e64e
Tommy CHONG <tommy.chong@ha.org.hk>
2020-03-21 13:37:41 +0800

    feat: add type for model

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
 export * from './common';
-export * from './interfaces';
 export * from './dynamoose.module';
+export * from './interfaces';
--- lib/interfaces/model-options.interface.ts
+++ lib/interfaces/model-options.interface.ts
@@ -1,4 +1,29 @@
 export type ModelOptions = {
   create?: boolean;
+  throughput?:
+    | {
+        read?: number;
+        write?: number;
+      }
+    | number
+    | string;
   prefix?: string;
+  suffix?: string;
+  waitForActive?: {
+    enabled?: boolean;
+    check?: {
+      timeout?: number;
+      frequency?: number;
+    };
+  };
+  update?: boolean;
+  expires?:
+    | {
+        ttl?: number;
+        attribute?: string;
+        items?: {
+          returnExpired?: boolean;
+        };
+      }
+    | number;
 };
