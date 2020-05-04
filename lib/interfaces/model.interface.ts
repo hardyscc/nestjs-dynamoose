27fa7cf52be9538a547976abd89cc63fd7a416a5
Hardys <hardyscc@gmail.com>
2020-05-04 22:57:37 +0800

    feat: update model interface to match dynamoose v2

--- lib/interfaces/model.interface.ts
+++ lib/interfaces/model.interface.ts
@@ -1,267 +1,162 @@
-type PopulateOptions = {
-  path: string;
-  model: string;
-  populate?: PopulateOptions;
-};
+import { AWSError } from 'aws-sdk';
+import {
+  Condition,
+  ConditionFunction,
+  ConditionInitalizer,
+} from 'dynamoose/dist/Condition';
 
-export interface PutOptions {
-  overwrite?: boolean;
-  updateTimestamps?: boolean;
-  updateExpires?: boolean;
-  condition?: string;
-  conditionNames?: any;
-  conditionValues?: any;
-}
+export declare type CallbackType<R, E> = (
+  error?: E | null,
+  response?: R,
+) => void;
 
-export interface Model<DataSchema, KeySchema> {
-  new (value?: DataSchema): ModelSchema<DataSchema>;
-  (value?: DataSchema): ModelSchema<DataSchema>;
-  readonly prototype: ModelSchema<DataSchema>;
-
-  batchPut(
-    items: DataSchema[],
-    options?: PutOptions,
-    callback?: (err: Error, items: ModelSchema<DataSchema>[]) => void,
-  ): Promise<ModelSchema<DataSchema>[]>;
-  batchPut(
-    items: DataSchema[],
-    callback?: (err: Error, items: ModelSchema<DataSchema>[]) => void,
-  ): Promise<ModelSchema<DataSchema>[]>;
-
-  create(
-    item: DataSchema,
-    options?: PutOptions,
-    callback?: (err: Error, model: ModelSchema<DataSchema>) => void,
-  ): Promise<ModelSchema<DataSchema>>;
-  create(
-    item: DataSchema,
-    callback?: (err: Error, model: ModelSchema<DataSchema>) => void,
-  ): Promise<ModelSchema<DataSchema>>;
-  create(
-    item: DataSchema,
-    options?: PutOptions,
-  ): Promise<ModelSchema<DataSchema>>;
-
-  get(
-    key: KeySchema,
-    callback?: (err: Error, data: DataSchema) => void,
-  ): Promise<ModelSchema<DataSchema> | undefined>;
-  batchGet(
-    key: KeySchema[],
-    callback?: (err: Error, data: DataSchema) => void,
-  ): Promise<ModelSchema<DataSchema>[]>;
-
-  delete(key: KeySchema, callback?: (err: Error) => void): Promise<undefined>;
-  batchDelete(
-    keys: KeySchema[],
-    callback?: (err: Error) => void,
-  ): Promise<undefined>;
-
-  query(
-    query?: QueryFilter,
-    callback?: (err: Error, results: ModelSchema<DataSchema>[]) => void,
-  ): QueryInterface<
-    ModelSchema<DataSchema>,
-    QueryResult<ModelSchema<DataSchema>>
-  >;
-  queryOne(
-    query?: QueryFilter,
-    callback?: (err: Error, results: ModelSchema<DataSchema>) => void,
-  ): QueryInterface<ModelSchema<DataSchema>, ModelSchema<DataSchema>>;
-  scan(
-    filter?: ScanFilter,
-    callback?: (err: Error, results: ModelSchema<DataSchema>[]) => void,
-  ): ScanInterface<ModelSchema<DataSchema>>;
-
-  update(
-    key: KeySchema,
-    update: UpdateUpdate<DataSchema>,
-    options: UpdateOptions,
-    callback: (err: Error, items: ModelSchema<DataSchema>[]) => void,
-  ): void;
-  update(
-    key: KeySchema,
-    update: UpdateUpdate<DataSchema>,
-    callback: (err: Error, items: ModelSchema<DataSchema>[]) => void,
-  ): void;
-  update(
-    key: KeySchema,
-    update: UpdateUpdate<DataSchema>,
-    options?: UpdateOptions,
-  ): Promise<ModelSchema<DataSchema>>;
-
-  transaction: ModelTransactionConstructor<DataSchema, KeySchema>;
-}
-type ModelSchema<T> = T;
-
-/**
- * Update
- */
-
-/**
- * Updates and existing item in the table. Three types of updates: $PUT, $ADD, and $DELETE.
- * Put is the default behavior.
- */
-type UpdateUpdate<DataSchema> =
-  | Partial<DataSchema>
-  | { $PUT: Partial<DataSchema> }
-  | { $ADD: Partial<DataSchema> }
-  | { $DELETE: Partial<DataSchema> };
-
-export interface UpdateOptions {
-  allowEmptyArray?: boolean;
-  createRequired?: boolean;
-  updateTimestamps?: boolean;
-  returnValues?: 'NONE' | 'ALL_OLD' | 'UPDATED_OLD' | 'ALL_NEW' | 'UPDATED_NEW';
-  condition?: string;
-  conditionNames?: any;
-  conditionValues?: any;
-}
-
-/**
- * Query
- */
-type QueryFilter = any;
-export interface QueryInterface<T, R> {
-  exec(callback?: (err: Error, result: R) => void): Promise<R>;
-  all(delay?: number, max?: number): QueryInterface<T, R>;
-  where(rangeKey: string): QueryInterface<T, R>;
-  filter(filter: string): QueryInterface<T, R>;
-  and(): QueryInterface<T, R>;
-  or(): QueryInterface<T, R>;
-  not(): QueryInterface<T, R>;
-  null(): QueryInterface<T, R>;
-  eq(value: any): QueryInterface<T, R>;
-  lt(value: any): QueryInterface<T, R>;
-  le(value: any): QueryInterface<T, R>;
-  ge(value: any): QueryInterface<T, R>;
-  gt(value: any): QueryInterface<T, R>;
-  beginsWith(value: string): QueryInterface<T, R>;
-  between(valueA: any, valueB: any): QueryInterface<T, R>;
-  contains(value: string): QueryInterface<T, R>;
-  in(values: any[]): QueryInterface<T, R>;
-  limit(limit: number): QueryInterface<T, R>;
-  consistent(): QueryInterface<T, R>;
-  descending(): QueryInterface<T, R>;
-  ascending(): QueryInterface<T, R>;
-  startAt(key: QueryKey): QueryInterface<T, R>;
-  attributes(attributes: string[]): QueryInterface<T, R>;
-  count(): QueryInterface<T, R>;
-  counts(): QueryInterface<T, R>;
-  using(indexName: string): QueryInterface<T, R>;
-  /* v2 */
-  exists(): QueryInterface<T, R>;
-}
-export interface QueryResult<T> extends Array<T> {
+export interface DocumentRetrieverResponse<Data, Key> extends Array<Data> {
+  lastKey?: Key;
   count: number;
-  lastKey?: QueryKey;
-  queriedCount?: number;
-  timesQueried: number;
 }
-type QueryKey = any;
-
-/**
- * Scan
- */
-type ScanFilter = string | any;
-
-export interface ScanInterface<T> {
-  exec(
-    callback?: (err: Error, result: ScanResult<T>) => void,
-  ): Promise<ScanResult<T>>;
-  all(delay?: number, max?: number): ScanInterface<T>;
-  parallel(totalSegments: number): ScanInterface<T>;
-  using(indexName: string): ScanInterface<T>;
-  consistent(filter?: any): ScanInterface<T>;
-  where(filter: any): ScanInterface<T>;
-  filter(filter: any): ScanInterface<T>;
-  and(): ScanInterface<T>;
-  not(): ScanInterface<T>;
-  null(): ScanInterface<T>;
-  eq(value: any): ScanInterface<T>;
-  lt(value: any): ScanInterface<T>;
-  le(value: any): ScanInterface<T>;
-  ge(value: any): ScanInterface<T>;
-  gt(value: any): ScanInterface<T>;
-  beginsWith(value: any): ScanInterface<T>;
-  between(valueA: any, valueB: any): ScanInterface<T>;
-  contains(value: any): ScanInterface<T>;
-  in(value: any): ScanInterface<T>;
-  limit(limit: number): ScanInterface<T>;
-  startAt(key: ScanKey): ScanInterface<T>;
-  attributes(value: any): ScanInterface<T>;
-  count(): ScanInterface<T>;
-  counts(): ScanInterface<T>;
-  /* v2 */
-  exists(): ScanInterface<T>;
-}
-
-export interface ScanResult<ModelData> extends Array<ModelData> {
-  count: number;
-  lastKey?: ScanKey;
-  scannedCount?: number;
+export interface ScanResponse<Data, Key>
+  extends DocumentRetrieverResponse<Data, Key> {
+  scannedCount: number;
   timesScanned: number;
 }
-
-type ScanKey = any;
-
-export interface VirtualType {
-  (options: any, name: string);
-  applyVirtuals(model: any): void;
-  get(fn: any): any;
-  set(fn: any): any;
+export interface QueryResponse<Data, Key>
+  extends DocumentRetrieverResponse<Data, Key> {
+  queriedCount: number;
+  timesQueried: number;
 }
 
-/**
- * Transaction
- */
-export interface ModelTransactionConstructor<DataSchema, KeySchema> {
-  new (value?: DataSchema): ModelSchema<DataSchema>;
-  (value?: DataSchema): ModelSchema<DataSchema>;
-  readonly prototype: ModelSchema<DataSchema>;
-
-  create(
-    item: DataSchema,
-    options?: PutOptions,
-    callback?: (err: Error, model: ModelSchema<DataSchema>) => void,
-  ): Promise<ModelSchema<DataSchema>>;
-  create(
-    item: DataSchema,
-    callback?: (err: Error, model: ModelSchema<DataSchema>) => void,
-  ): Promise<ModelSchema<DataSchema>>;
-  create(
-    item: DataSchema,
-    options?: PutOptions,
-  ): Promise<ModelSchema<DataSchema>>;
-
-  get(
-    key: KeySchema,
-    callback?: (err: Error, data: DataSchema) => void,
-  ): Promise<ModelSchema<DataSchema> | undefined>;
-
-  delete(key: KeySchema, callback?: (err: Error) => void): Promise<undefined>;
-
-  update(
-    key: KeySchema,
-    update: UpdateUpdate<DataSchema>,
-    options: UpdateOptions,
-    callback: (err: Error, items: ModelSchema<DataSchema>[]) => void,
-  ): void;
-  update(
-    key: KeySchema,
-    update: UpdateUpdate<DataSchema>,
-    callback: (err: Error, items: ModelSchema<DataSchema>[]) => void,
-  ): void;
-  update(
-    key: KeySchema,
-    update: UpdateUpdate<DataSchema>,
-    options?: UpdateOptions,
-  ): Promise<ModelSchema<DataSchema>>;
-
-  conditionCheck(key: KeySchema, options?: ConditionOptions): void;
+export interface UnprocessedItems<T> {
+  unprocessedItems: T[];
 }
-export interface ConditionOptions {
-  condition: string;
-  conditionNames: object;
-  conditionValues: object;
+
+export enum SortOrder {
+  ascending = 'ascending',
+  descending = 'descending',
+}
+
+export interface ModelUpdateSettings {
+  condition?: Condition;
+}
+
+export type UpdatePartial<T> =
+  | Partial<T>
+  | { $SET: Partial<T> }
+  | { $ADD: Partial<T> }
+  | { $REMOVE: Partial<T> };
+
+export interface Model<Data, Key> {
+  query(condition?: ConditionInitalizer): QueryInterface<Data, Key>;
+  scan(condition?: ConditionInitalizer): ScanInterface<Data, Key>;
+
+  update(obj: Data): Promise<Data>;
+  update(obj: Data, callback: CallbackType<Data, AWSError>): void;
+
+  update(keyObj: Key, updateObj: UpdatePartial<Data>): Promise<Data>;
+  update(
+    keyObj: Key,
+    updateObj: UpdatePartial<Data>,
+    callback: CallbackType<Data, AWSError>,
+  ): void;
+  update(
+    keyObj: Key,
+    updateObj: UpdatePartial<Data>,
+    settings: ModelUpdateSettings,
+  ): Promise<Data>;
+  update(
+    keyObj: Key,
+    updateObj: UpdatePartial<Data>,
+    settings: ModelUpdateSettings,
+    callback: CallbackType<Data, AWSError>,
+  ): void;
+
+  create(document: Data): Promise<Data>;
+  create(document: Data, callback: CallbackType<Data, Error>): void;
+
+  delete(key: Key): Promise<void>;
+  delete(key: Key, callback: CallbackType<void, AWSError>): void;
+
+  get(key: Key): Promise<Data>;
+  get(key: Key, callback: CallbackType<Data, AWSError>): void;
+
+  batchPut(documents: Data[]): Promise<UnprocessedItems<Data>>;
+  batchPut(
+    documents: Data[],
+    callback: CallbackType<UnprocessedItems<Data>, AWSError>,
+  ): void;
+
+  batchGet(keys: Key[]): Promise<Data[]>;
+  batchGet(keys: Key[], callback: CallbackType<Data[], AWSError>): void;
+
+  batchDelete(keys: Key[]): Promise<UnprocessedItems<Key>>;
+  batchDelete(
+    keys: Key[],
+    callback: CallbackType<UnprocessedItems<Key>, AWSError>,
+  ): void;
+}
+
+export interface QueryInterface<Data, Key> {
+  exec(): Promise<QueryResponse<Data, Key>>;
+  exec(callback: CallbackType<QueryResponse<Data, Key>, AWSError>): void;
+
+  getRequest(): Promise<any>;
+  all(delay?: number, max?: number): QueryInterface<Data, Key>;
+  limit(value: number): QueryInterface<Data, Key>;
+  startAt(value: Key): QueryInterface<Data, Key>;
+  attributes(value: string[]): QueryInterface<Data, Key>;
+  count(): QueryInterface<Data, Key>;
+  consistent(): QueryInterface<Data, Key>;
+  using(value: string): QueryInterface<Data, Key>;
+  and(): QueryInterface<Data, Key>;
+  or(): QueryInterface<Data, Key>;
+  not(): QueryInterface<Data, Key>;
+  parenthesis(value: Condition | ConditionFunction): QueryInterface<Data, Key>;
+  group(value: Condition | ConditionFunction): QueryInterface<Data, Key>;
+  where(key: string): QueryInterface<Data, Key>;
+  filter(key: string): QueryInterface<Data, Key>;
+  attribute(key: string): QueryInterface<Data, Key>;
+  eq(value: any): QueryInterface<Data, Key>;
+  lt(value: number): QueryInterface<Data, Key>;
+  le(value: number): QueryInterface<Data, Key>;
+  gt(value: number): QueryInterface<Data, Key>;
+  ge(value: number): QueryInterface<Data, Key>;
+  beginsWith(value: any): QueryInterface<Data, Key>;
+  contains(value: any): QueryInterface<Data, Key>;
+  exists(value: any): QueryInterface<Data, Key>;
+  in(value: any): QueryInterface<Data, Key>;
+  between(...values: any[]): QueryInterface<Data, Key>;
+  sort(order: SortOrder): QueryInterface<Data, Key>;
+}
+
+export interface ScanInterface<Data, Key> {
+  exec(): Promise<ScanResponse<Data, Key>>;
+  exec(callback: CallbackType<ScanResponse<Data, Key>, AWSError>): void;
+  parallel(value: number): ScanInterface<Data, Key>;
+
+  getRequest(): Promise<any>;
+  all(delay?: number, max?: number): ScanInterface<Data, Key>;
+  limit(value: number): ScanInterface<Data, Key>;
+  startAt(value: Key): ScanInterface<Data, Key>;
+  attributes(value: string[]): ScanInterface<Data, Key>;
+  count(): ScanInterface<Data, Key>;
+  consistent(): ScanInterface<Data, Key>;
+  using(value: string): ScanInterface<Data, Key>;
+  and(): ScanInterface<Data, Key>;
+  or(): ScanInterface<Data, Key>;
+  not(): ScanInterface<Data, Key>;
+  parenthesis(value: Condition | ConditionFunction): ScanInterface<Data, Key>;
+  group(value: Condition | ConditionFunction): ScanInterface<Data, Key>;
+  where(key: string): ScanInterface<Data, Key>;
+  filter(key: string): ScanInterface<Data, Key>;
+  attribute(key: string): ScanInterface<Data, Key>;
+  eq(value: any): ScanInterface<Data, Key>;
+  lt(value: number): ScanInterface<Data, Key>;
+  le(value: number): ScanInterface<Data, Key>;
+  gt(value: number): ScanInterface<Data, Key>;
+  ge(value: number): ScanInterface<Data, Key>;
+  beginsWith(value: any): ScanInterface<Data, Key>;
+  contains(value: any): ScanInterface<Data, Key>;
+  exists(value: any): ScanInterface<Data, Key>;
+  in(value: any): ScanInterface<Data, Key>;
+  between(...values: any[]): ScanInterface<Data, Key>;
+  sort(order: SortOrder): ScanInterface<Data, Key>;
 }
